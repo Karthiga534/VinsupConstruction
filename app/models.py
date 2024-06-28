93967aca4ac3a86611ecbbfc437c69f96654bc68
dharshini <dharshinispriya1710@gmail.com>
2024-06-28 17:40:48 +0530

    rgeg

--- app/models.py
+++ app/models.py
@@ -1207,7 +1207,7 @@ class ProjectScheduleHistory(models.Model):
     qty = models.IntegerField(default=0, null=True, blank=True)
     unit = models.ForeignKey(Uom, on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField(null=True,blank=True)
-    img =models.FileField(upload_to="doc",null=True,blank=True)
+    # img =models.FileField(upload_to="doc",null=True,blank=True)
 
     def __str__(self):
         return f'{self.work} - {self.project_schedule}'
